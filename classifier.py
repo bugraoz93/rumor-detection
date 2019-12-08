5c9bdeefc69eddde32e9bed147e1a6f6c7a198c0
Buğra Öztürk <ozturkbugra93@gmail.com>
2019-12-08 21:34:50 +0300

    Elasticsearch

--- classifier.py
+++ classifier.py
@@ -1,9 +1,14 @@
+import copy
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_recall_fscore_support as score
+import matplotlib.pyplot as plt
 
 from PhemeDataset import PhemeDatasetES
 
@@ -17,31 +22,27 @@ test_set = dataset.get_source_tweet_representations("ferguson")
 X = []
 y = []
 for representation in representations:
-    x = [representation['time_span'],
-         representation['early_reaction_count'],
-         representation['mid_reaction_count'],
-         representation['all_reaction_count'],
-         representation['is_sensitive'],
+    x = [representation['time_span'],  # 0
+         representation['early_reaction_count'],  # 1
+         representation['mid_reaction_count'],  # 2
+         representation['all_reaction_count'],  # 3
+         representation['is_sensitive'],  # 4
          # User
-         representation['is_geo_enabled'],
-         representation['has_description'],
-         representation['description_word_count'],
-         representation['role_score'],
-         representation['user_follower_count'],
-         representation['is_verified'],
-         representation['favorites_count'],
+         representation['is_geo_enabled'],  # 5
+         representation['has_description'],  # 6
+         representation['description_word_count'],  # 7
+         representation['role_score'],  # 8
+         representation['user_follower_count'],  # 9
+         representation['is_verified'],  # 10
+         representation['favorites_count'],  # 11
          # Tweet
-         representation['has_question_mark'],
-         representation['question_mark_count'],
-         representation['has_exclamation_mark'],
-         representation['exclamation_mark_count'],
-         representation['has_dotdotdot_mark'],
-         representation['dotdotdot_mark_count']
+         representation['has_question_mark'],  # 12
+         representation['question_mark_count'],  # 13
+         representation['has_exclamation_mark'],  # 14
+         representation['exclamation_mark_count'],  # 15
+         representation['has_dotdotdot_mark'],  # 16
+         representation['dotdotdot_mark_count']  # 17
 
-         # representation['reaction_speed'],
-         # representation['reaction_mention_count'],
-         # representation['reaction_retweet_count'],
-         # representation['user_event_time_diff']
          ]
     X.append(x.copy())
     y.append(representation['isRumor'])
@@ -69,10 +70,6 @@ for representation in test_set:
          representation['has_dotdotdot_mark'],
          representation['dotdotdot_mark_count']
 
-         # representation['reaction_speed'],
-         # representation['reaction_mention_count'],
-         # representation['reaction_retweet_count'],
-         # representation['user_event_time_diff']
          ]
     X_test.append(x.copy())
     y_test.append(representation['isRumor'])
@@ -83,56 +80,109 @@ X_train = X
 y_train = y
 
 
-def classify_with_rfc(X, y):
+def classify_with_rfc(train_data, train_data_y, test_data, test_data_y):
     cls = RandomForestClassifier()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    cls.fit(X_train, y_train)
-    accuracy = cls.score(X_test, y_test)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    cls.fit(train_data, train_data_y)
+    accuracy = cls.score(test_data, test_data_y)
     print("Accuracy: " + str(accuracy))
     return accuracy, cls
 
 
-def classify_with_linear_regression(X, y):
+def classify_with_linear_regression(train_data, train_data_y, test_data, test_data_y):
     lr = LinearRegression()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    lr.fit(X_train, y_train)
-    accuracy = lr.score(X_test, y_test)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    lr.fit(train_data, train_data_y)
+    accuracy = lr.score(test_data, test_data_y)
     print("Accuracy: " + str(accuracy))
     return accuracy, lr
 
 
-def classify_with_svm(X, y):
+def classify_with_svm(train_data, train_data_y, test_data, test_data_y):
     svc = SVC()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    svc.fit(X_train, y_train)
-    accuracy = svc.score(X_test, y_test)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    svc.fit(train_data, train_data_y)
+    accuracy = svc.score(test_data, test_data_y)
     print("Accuracy: " + str(accuracy))
     return accuracy, svc
 
 
-def classify_with_dt(X, y):
+def classify_with_dt(train_data, train_data_y, test_data, test_data_y):
     svc = DecisionTreeClassifier(random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    svc.fit(X_train, y_train)
-    accuracy = svc.score(X_test, y_test)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    svc.fit(train_data, train_data_y)
+    accuracy = svc.score(test_data, test_data_y)
     print("Accuracy: " + str(accuracy))
     return accuracy, svc
 
 
-def evaluate_model(model, X_test, y_test):
+precisions_rf = list()
+precisions_svm = list()
+precisions_dt = list()
+
+recall_rf = list()
+recall_svm = list()
+recall_dt = list()
+
+
+def evaluate_model(model, X_test, y_test, type):
     predicted_y = model.predict(X_test)
     report = classification_report(y_test, predicted_y)
+    matrix = confusion_matrix(y_test, predicted_y)
+    precision, recall, fscore, support = score(y_test, predicted_y, average='macro')
+    if type == "rf":
+        precisions_rf.append(precision)
+        recall_rf.append(recall)
+    elif type == "svm":
+        precisions_svm.append(precision)
+        recall_svm.append(recall)
+    else:
+        precisions_dt.append(precision)
+        recall_dt.append(recall)
     print(report)
+    print(matrix)
 
 
-print("Random Forrest: ")
-_, model = classify_with_rfc(X_train, y_train)
-evaluate_model(model, X_test, y_test)
+for i in range(3, 17):
+    print("For the K-Selected feature value: " + str(i))
+    selector = SelectKBest(chi2, k=i)
+    X_train_new = selector.fit_transform(X.copy(), y.copy()).tolist()
+    selected_features = selector.get_support(indices=True)
+    print("Selected Feature Indices: " + str(selected_features))
+    X_test_new = copy.deepcopy(X_test)
+    for data_num in range(len(X_test_new)):
+        X_test_new[data_num] = [i for j, i in enumerate(X_test_new[data_num]) if j in selected_features]
 
-print("SVM: ")
-_, model = classify_with_svm(X_train, y_train)
-evaluate_model(model, X_test, y_test)
+    print("Random Forrest: ")
+    _, model = classify_with_rfc(X_train_new, y, X_test_new, y_test)
+    evaluate_model(model, X_test_new, y_test, "rf")
 
-print("Decision Tree: ")
-_, model = classify_with_dt(X_train, y_train)
-evaluate_model(model, X_test, y_test)
+    print("SVM: ")
+    _, model = classify_with_svm(X_train_new, y, X_test_new, y_test)
+    evaluate_model(model, X_test_new, y_test, "svm")
+
+    print("Decision Tree: ")
+    _, model = classify_with_dt(X_train_new, y, X_test_new, y_test)
+    evaluate_model(model, X_test_new, y_test, "dt")
+    print("----------------------------------------------------")
+
+plt.ylabel('Precision for RF')
+plt.xlabel('K-Selected Features')
+plt.plot(range(3, 17), precisions_rf, label='Precision', marker='o')
+plt.plot(range(3, 17), recall_rf, label='Recall', marker='x')
+plt.legend()
+plt.show()
+
+plt.ylabel('Precision for SVM')
+plt.xlabel('K-Selected Features')
+plt.plot(range(3, 17), precisions_svm, label='Precision', marker='o')
+plt.plot(range(3, 17), recall_svm, label='Recall', marker='x')
+plt.legend()
+plt.show()
+
+plt.ylabel('Precision for DT')
+plt.xlabel('K-Selected Features')
+plt.plot(range(3, 17), precisions_dt, label='Precision', marker='o')
+plt.plot(range(3, 17), recall_dt, label='Recall', marker='x')
+plt.legend()
+plt.show()
